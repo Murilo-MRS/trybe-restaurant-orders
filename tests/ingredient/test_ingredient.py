0a2c93bdf3f4d7b3eaceeead4bb1c6cbb397a90f
Murilo-MRS <murilosantana1993@gmail.com>
2023-06-16 14:56:20 -0300

    test: req 1 - classe Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,7 +1,23 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
-    # Ingredient("name", "unit", 1.0)
+    ingredient_meat = Ingredient("carne")
+    ingredient_1 = Ingredient("carne")
+    ingredient_2 = Ingredient("carne")
+    ingredient_3 = Ingredient("ovo")
+
+    assert ingredient_meat.name == "carne"
+    assert ingredient_meat.restrictions == {
+        Restriction.ANIMAL_MEAT,
+        Restriction.ANIMAL_DERIVED,
+    }
+    assert repr(ingredient_meat) == "Ingredient('carne')"
+    assert ingredient_1 == ingredient_2
+    assert ingredient_1 != ingredient_3
+    assert hash(ingredient_1) == hash(ingredient_2)
+    assert hash(ingredient_1) != hash(ingredient_3)
